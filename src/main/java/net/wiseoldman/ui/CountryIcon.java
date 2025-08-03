58366fd987c2a91f4b0aaf3e7f52141db07dd70e
Robin <5983417+rorro@users.noreply.github.com>
2025-08-03 22:49:25 +0200

    Add England country flag

--- src/main/java/net/wiseoldman/ui/CountryIcon.java
+++ src/main/java/net/wiseoldman/ui/CountryIcon.java
@@ -83,6 +83,7 @@ public enum CountryIcon
 	ECUADOR("ec", "1f1ea-1f1e8"),
 	EGYPT("eg", "1f1ea-1f1ec"),
 	EL_SALVADOR("sv", "1f1f8-1f1fb"),
+	ENGLAND("gb_eng", "1f3f4-e0067-e0062-e0065-e006e-e0067-e007f"),
 	EQUATORIAL_GUINEA("gq", "1f1ec-1f1f6"),
 	ERITREA("er", "1f1ea-1f1f7"),
 	ESTONIA("ee", "1f1ea-1f1ea"),
